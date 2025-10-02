04bd4e2b34ca8c064599cccd280f2e7743e35e15
njprem <prem842547@gmail.com>
2025-10-02 21:24:51 +0700

    Footer Component

--- src/pages/Traveler/LandingPage/LandingPage.tsx
+++ src/pages/Traveler/LandingPage/LandingPage.tsx
@@ -1,11 +1,10 @@
-import Navbar from "../../../components/navbar"; 
+import Navbar from "../../../components/navbar";
 import Footer from "../../../components/footer";
 
 export const LandingPage = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-
       <main className="flex-1 p-6">
         {/* page content */}
       </main>
