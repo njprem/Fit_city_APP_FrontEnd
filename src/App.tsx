b2fbff69bfbf8e3ca2b385983998901ec73febb2
njprem <prem842547@gmail.com>
2025-10-08 17:44:04 +0700

    Comment Sarah fogotpassword page and component to test jenkin

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { LandingPage } from './pages/Traveler/LandingPage/LandingPage';
 import LoginPage from "./pages/Traveler/LandingPage/LogInPage";
 import SignUpPage from "./pages/Traveler/LandingPage/SignupPage";
 import TermandCondition from './pages/Traveler/LandingPage/TermandCondition';
-import ForgotPassword from './pages/Traveler/LandingPage/ForgotPassword';
+// import ForgotPassword from './pages/Traveler/LandingPage/ForgotPassword';
 
 export default function App() {
   return (
@@ -15,7 +15,7 @@ export default function App() {
             <Route path="/login" element={<LoginPage/>} />
             <Route path="/signup" element={<SignUpPage/>} />
             <Route path="/terms" element={<TermandCondition/>} />
-            <Route path="/forgot-password" element={<ForgotPassword/>} />
+            {/* <Route path="/forgot-password" element={<ForgotPassword/>} /> */}
           </Routes>
       </Router>
    </>
