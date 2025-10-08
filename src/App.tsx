3a20788f3dd8359be37777598f68b561bb7fd4b6
Phuwasin <fluke.craft@gmail.com>
2025-10-08 17:18:26 +0700

    uwawawaawa

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage } from './pages/Traveler/LandingPage/LandingPage';
-<<<<<<< HEAD
 import ProfilePage from "./pages/Traveler/LandingPage/Profile_Page";
-=======
 import LoginPage from "./pages/Traveler/LandingPage/LogInPage";
 import SignUpPage from "./pages/Traveler/LandingPage/SignupPage";
 import TermandCondition from './pages/Traveler/LandingPage/TermandCondition';
 import ForgotPassword from './pages/Traveler/LandingPage/ForgotPassword';
->>>>>>> 78099543ea5f099364d84b4f33f890755ed32325
 
 export default function App() {
   return (
@@ -16,14 +13,11 @@ export default function App() {
       <Router>
           <Routes>
             <Route path= "/" element = {<LandingPage/>}/>
-<<<<<<< HEAD
             <Route path="/profile" element={<ProfilePage />} />
-=======
             <Route path="/login" element={<LoginPage/>} />
             <Route path="/signup" element={<SignUpPage/>} />
             <Route path="/terms" element={<TermandCondition/>} />
             <Route path="/forgot-password" element={<ForgotPassword/>} />
->>>>>>> 78099543ea5f099364d84b4f33f890755ed32325
           </Routes>
       </Router>
    </>
